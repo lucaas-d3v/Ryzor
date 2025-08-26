a905e6d889986c07273ecebffc82904bccdb2fbe
The_K <lucas.workps@gmail.com>
2025-08-26 19:00:11 -0300

    Atualizar o core.py

--- src/core.py
+++ src/core.py
@@ -4,40 +4,71 @@ from pathlib import Path
 import shutil as sh
 import json
 from os import name, system
+                            
+def realocate_files(entrada: Path, saida: Path, backup: bool = False, preview: bool = False, verbose: bool = False) -> bool:
+    """ Função principal, papel: fazer backup/organizar os arquivo
+    Função principal do Ryzor
+    
+    args:
+        entrada: caminho de entrada dos arquivos a serem organizados/backup.
+        saida: caminho de saída onde os arquivos seram levados ao fim do processo.
+        backup: informa será feito o backup pu apenas organização.
+        preview: informa se o usuário quer o preview de tudo antes da ação.
+        verbose: informa será o usuário quer ssber literalmente tudo, caminhos completos e demais.
+    
+    returns:
+        retorna False caso o caminho seja um caminho ou n exista, etc, ou True caso tudo ocorra como esperado.
+    """
 
-def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = False, verbose: bool = False) -> bool:
-    """ Função principal, papel: fazer backup/organizar os arquivos."""
-
-    """Verifica se o diretório de entrada realmente existe."""
     if not entrada.exists():
+        """
+        Medida de segurança caso o diretório informado não exista.
+        """
+        
         return False
 
-    """Verifica se o diretório de entrada realmente é um diretório."""
     if not entrada.is_dir():
+        """
+        Medida de segurança caso o caminho especificado não seja um diretório.
+        """
+        
         return False
 
-    """Verifica de o diretório de saída realmente existe."""
     if not saida.exists():
+        """
+        Medida de segurança caso a saida não exista.
+        """
 
-        """Cria caso não exista"""
         saida.mkdir(parents=True, exist_ok=True)
 
-        """Caso, de alguma forma o diretorio de saída ainda n exista, retorna False."""
-        if not saida.exists():
-            return False
-
-    """Verifica se o diretório de saída, realmente é um diretório."""
     if not saida.is_dir():
+        """
+        Medida de segurança, caso a saida não seja um diretório.
+        """
+        
         return False
-    
-    def continuar() -> bool:
-        aproveds = ["s", "yes", "sim", "ok"]
-        c = input("Deseja continuar? (s/n): ").lower()
 
+    def continuar() -> bool:
+        """
+        Para evitar chamadas duplicadas, criei continuar() para perguntar ao usuário se quer confirmar a ação.
+        """
+        
+        aproveds = ["s", "yes", "sim", "ok"]
+        c = input("Deseja continuar? (s/n): ").lower().trim()
+        
         return c in aproveds
 
     def execute(mudancas: dict[str, str]):
-        """Executa as mudanças solicitadas"""
+        """
+        Criei execute() para evitar repetição de código.
+        
+        args:
+            mudancas: um dicionário com chave str, e valor str,
+                      basicamente, o caminho onde o arquivo está, é onde ele deve ser levado.
+        
+        returns:
+            retorna True caso as mudanças ocoram como esperadas.
+        """
 
         try:
             if backup:
@@ -51,15 +82,15 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
 
                     if verbose:
                         print(f"[Ryzor] Copiando {_arquivo} -> {_destino}")
-                    
+
                     else:
                         print(f"[Ryzor] Copiando {sep.join(_arquivo.parts[-2:])} -> {sep.join(_destino.parts[-2:])}")
-                    
 
-                    sh.copy2(arquivo, destino)  
-                
+
+                    sh.copy2(arquivo, destino)
+
                 else:
-                    return True    
+                    return True
 
             else:
                 for arquivo, destino in mudancas:
@@ -71,27 +102,30 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
 
                     if verbose:
                         print(f"[Ryzor] Movendo {_arquivo} -> {_destino}")
-                    
+
                     else:
                         print(f"[Ryzor] Movendo {sep.join(_arquivo.parts[-2:])} -> {sep.join(_destino.parts[-2:])}")
-                    
-                    sh.move(arquivo, destino)  
-                
+
+                    sh.move(arquivo, destino)
+
                 else:
                     return True
-                
+
         except Exception as e:
             print("[Ryzor] Erro:", e)
             return False
 
 
     def not_in_backup_folder(arquivo: Path) -> bool:
-        """Verifica se a pasta é de backup"""
+        """
+        Criei not_in_backup_folder(), para verificar se a pasta é de backup,
+        assim, evitando fazer backup, de uma pasta de bakcup
+        """
+        
         return "backup" not in [p.lower() for p in arquivo.parts[:-1]]
 
-    
     sep = '/' if name != 'nt' else '\\'
-    
+
     try:
         system("cls" if name == "nt" else "clear")
 
@@ -102,9 +136,9 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
             base_dir = Path(__file__).parent
             json_path = base_dir / "extensions.json"
 
-            with json_path.open("r", encoding="utf-8") as f:
+            with json_path.open("r", encoding="utf-8-sig") as f:
                 tipos_de_arquivos = json.load(f)
-        
+
         except (FileNotFoundError, json.JSONDecodeError) as e:
             print(f"[Ryzor] Erro ao carregar JSON: {e}")
             return False
@@ -118,7 +152,7 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
                 f"""\t\t\t\tComeçando em {entrada} ...
                 \t\tModo: {'Backup' if backup else 'Organização'}\n
         Atual {'-' * 17} Pós-mudanças\n""")
-        
+
         else:
             print(
                 f"""Começando em {entrada} ...
@@ -132,12 +166,12 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
 
             for tipo, extensoes in tipos_de_arquivos.items():
                 if any(arquivo.suffix.lower().endswith(ext) for ext in extensoes):
-                    pasta_destino = Path(saida / tipo) 
+                    pasta_destino = Path(saida / tipo)
                     break
-        
+
             else:
                 pasta_destino = Path(f"{saida}/Sem_Extensões")
-            
+
             pasta_destino.mkdir(parents=True, exist_ok=True)
             destino = pasta_destino / arquivo.name
 
@@ -147,7 +181,7 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
                         print(f"[Ryzor] {arquivo} -> {destino}")
                     else:
                         print(f"[Ryzor] {sep.join(arquivo.parts[-2:])} -> {sep.join(destino.parts[-2:])}")
-                
+
                 arquivos_a_mudar[str(arquivo)] = str(destino)
 
             else:
@@ -157,18 +191,17 @@ def organizar(entrada: Path, saida: Path, backup: bool = False, preview: bool = 
                     else:
                         print(f"[Ryzor] {sep.join(arquivo.parts[-2:])} -> {sep.join(destino.parts[-2:])}")
 
-                
+
                 arquivos_a_mudar[str(arquivo)] = str(destino)
 
         else:
             if continuar():
                 return execute(arquivos_a_mudar)
-    
-            
+
+
         print("[Ryzor] Cancelando...")
         return False
-    
+
     except Exception as e:
         print(f"Erro: {e}")
         return False
-    
